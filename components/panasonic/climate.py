3b5a0cac4b4a47304130655cf7e0def6f98828db
Dmitry Kochetov <dkochetov@inbox.ru>
2025-09-30 20:47:38 +0300

    Use new climate_ir schema helper

--- components/panasonic/climate.py
+++ components/panasonic/climate.py
@@ -8,6 +8,7 @@ from esphome.const import (
 from esphome.components.climate import (
     ClimateSwingMode,
 )
+
 ALLOWED_CLIMATE_SWING_MODES = {
     "AUTO": ClimateSwingMode.CLIMATE_SWING_AUTO,
     "VERTICAL": ClimateSwingMode.CLIMATE_SWING_VERTICAL,
@@ -19,16 +20,15 @@ AUTO_LOAD = ["climate_ir"]
 panasonic_ns = cg.esphome_ns.namespace("panasonic")
 PanasonicClimate = panasonic_ns.class_("PanasonicClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(PanasonicClimate).extend(
     {
-        cv.GenerateID(): cv.declare_id(PanasonicClimate),
-        cv.Optional(CONF_SUPPORTED_SWING_MODES): cv.ensure_list(
-        validate_swing_modes
-        ),
+        cv.Optional(CONF_SUPPORTED_SWING_MODES): cv.ensure_list(validate_swing_modes),
     }
 )
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
     if CONF_SUPPORTED_SWING_MODES in config:
         cg.add(var.set_supported_swing_modes(config[CONF_SUPPORTED_SWING_MODES]))
+
