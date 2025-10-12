ec36ba6a378dc8b98f1ae25f32c539dc0ec31d88
Dmitry Kochetov <dkochetov@inbox.ru>
2025-10-12 14:47:50 +0300

    simplify

--- components/panasonic/climate.py
+++ components/panasonic/climate.py
@@ -1,32 +1,13 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import (
-    CONF_SUPPORTED_SWING_MODES,
-)
-from esphome.components.climate import (
-    ClimateSwingMode,
-)
-
-ALLOWED_CLIMATE_SWING_MODES = {
-    "AUTO": ClimateSwingMode.CLIMATE_SWING_AUTO,
-    "VERTICAL": ClimateSwingMode.CLIMATE_SWING_VERTICAL,
-}
-validate_swing_modes = cv.enum(ALLOWED_CLIMATE_SWING_MODES, upper=True)
 
 AUTO_LOAD = ["climate_ir"]
 
 panasonic_ns = cg.esphome_ns.namespace("panasonic")
 PanasonicClimate = panasonic_ns.class_("PanasonicClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(PanasonicClimate).extend(
-    {
-        cv.Optional(CONF_SUPPORTED_SWING_MODES): cv.ensure_list(validate_swing_modes),
-    }
-)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(PanasonicClimate)
+
 
 async def to_code(config):
-    var = await climate_ir.new_climate_ir(config)
-    if CONF_SUPPORTED_SWING_MODES in config:
-        cg.add(var.set_supported_swing_modes(config[CONF_SUPPORTED_SWING_MODES]))
-
+    await climate_ir.new_climate_ir(config)
